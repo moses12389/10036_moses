f9afd21bffd713b0e9775364f83ec79e51f2ee5b
Moses10036 <moseswanjala670@gmail.com>
2026-06-17 22:39:42 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,25 @@
+let item=item1
+let item=item2
+let item=item3
+let item=item4
+let total=totals
+let vat=5%
+let paid= 4000
+let change=1300
+let servedby=moses
+Document.getElementById("item1")
+innerHTML=item1
+Document.getElementById("item2")
+innerHTML=item2Document.getElementById("item3")
+innerHTML=item3
+Document.getElementById("item4")
+innerHTML=item4
+Document.getElementById("total")
+innerHTML=totalDocument.getElementById("VAT")
+innerHTML=VAT
+Document.getElementById("paid")
+
+Document.getElementById("change")
+innerHTML=change
+Document.getElementById("servedby")
+innerHTML=servedby
