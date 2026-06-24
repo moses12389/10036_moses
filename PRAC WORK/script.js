9fe02ad5292093f612a9a0599b49ae0c76363de1
unknown <KONZA-VDI@GROUP16-059.vdi.konza>
2026-06-24 12:11:33 +0300

    orgarnized folders

---
+++ PRAC WORK/script.js
@@ -0,0 +1,36 @@
+let popularonNetflix=
+["moneyheist","narco","ozar"]
+console.log(popularonNetflix[0])
+console.log(popularonNetflix[1])
+console.log(popularonNetflix[2])
+
+
+
+let categories=
+   ["action","Drama","anime","lifestyle","homur","scifi"]
+    console.log(categories[0])
+    console.log(categories[1])
+    console.log(categories[2])
+    console.log(categories[3])
+    console.log(categories[3])
+    console.log(categories[5])
+    console.log(categories[6])
+let Fname="Moses"
+let Sname="wanjala"
+console.log(Fname+ " "+Sname)
+console.log(popularonNetflix[1]+" "+popularonNetflix[2])
+console.log(categories[4]+" "+categories[5])
+console.log(categories[5])+" "+(categories[6])
+
+let displayInterface={
+    title:"moneyheist",
+    rating:5.6,
+    season:4
+}
+
+console.log(displayInterface.title)
+console.log(displayInterface.rating)
+console.log(displayInterface.season)
+console.log(displayInterface)
+console.log(displayInterface .title+" "+categories[3]+" "+displayInterface .rating)
+
---
+++ PRAC WORK/wanjala.js
@@ -0,0 +1,13 @@
+var edible=bread
+console.log("bread")
+var  Edible=crisps
+console.log("crisps")
+var noneedible=plasticcup
+console.log("plastic cup")
+var machinery=motocycleEngine
+console.log("motocycle Engine")
+var machinery=electricSaw
+console.log("electricsaw")
+var machinery=eletricdrill
+console.log("electricdrill")
+
